fcda07c4e8e8698a505c9b6d36f6b9b4f813e7db
anfecruz <anfecruz@hotmail.com>
2025-06-05 17:36:05 -0500

    test: actualizar pruebas y configuración del pipeline

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'hola-mundo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hola-mundodefauld');
+    expect(app.title).toEqual('hola-mundo'); // Corregido el valor esperado
   });
 
   it('should render title', () => {
